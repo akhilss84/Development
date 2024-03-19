5bad11a121d1ede7684d0faf6e5db11b9818a3eb
akhilss84 <72548712+akhilss84@users.noreply.github.com>
2024-03-19 16:57:07 +0530

    Test (#9)
    * .
    
    * .
    
    * v.1.2
    
    * .
    
    * v.1.2
    
    * 1st snapshot
    
    ---------
    
    Co-authored-by: Akhil Singh <akhil.singh@kyndryl.com>

--- provider.tf
+++ provider.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.20.0"
     }
   }
+}
+
+provider "google" {
+  project = "active-valve-417311"
+  region = "us-eas4"
 }
